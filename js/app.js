da24ad15b29af6cfe4b23726aa1c1eadc4630292
JinWoo <bjw8518@gmail.com>
2023-06-07 14:43:05 +0900

    230607

--- js/app.js
+++ js/app.js
@@ -22,19 +22,31 @@ var connection = mysql.createConnection({
 });
 
 
-app.get('/posts',function(req, res){
-  connection.query('SELECT * FROM post;', function (error, results, fields) {
-    if(error) throw error;
-    res.send(results);
-  });
+app.get('/chart', function(req,res){
+  res.sendfile("./html/web_05_25/time_2.html")
 });
 
-app.post('/posts', function(req, res) {
-  let title = req.body.title;
-  let content = req.body.content;
-  let sql = "INSERT INTO post (title, content) VALUES (?, ?)";
-  connection.query(sql, [title, content], function (error, results, fields) {
-    if(error) throw error;
-    res.send(results);
-  });
+app.get('/chart_google', function(req,res){
+  res.sendfile("./html/web_05_25/chart.html")
+});
+
+app.get('/line_chart', function(req,res){
+  res.sendfile("./html/web_05_25/line_chart.html")
+});
+
+app.get('/line_chart_stock', function(req,res){
+  res.sendfile("./html/web_05_25/line_chart_stock.html")
+});
+
+app.get('/stock', function(req,res){
+  res.sendfile("./html/web_06_07/stock.html")
+});
+
+
+app.get('/getStockPrice', function(req,res){
+  const request = require('request');
+  request('https:https://polling.finance.naver.com/api/realtime?query=SERVICE_ITEM:328380|SERVICE_RECENT_ITEM:328380&_callback=window.__jindo2_callback._9993',)
+  console.error('error',error);
+  console.log('statusCode',response && response.statusCode);
+  console.log('body',JSON.parse(body.split("(")[1].split(")")[0]));
 });
