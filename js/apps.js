37c0898e6f997bff6040280a409d772da8b717e5
JinWoo <bjw8518@gmail.com>
2023-06-13 16:59:15 +0900

    230613

---
+++ js/apps.js
@@ -0,0 +1,39 @@
+// node_modules 에 있는 express 관련 파일을 가져온다.
+// var express = require('express');
+// require은 함수이므로, 변환값을 변수에 저장한다.
+// var http = require('http');
+// express는 함수이므로, 변환값을 변수에 저장한다.
+// var app = express();
+// 서버 만들기
+// 포트번호 변경 시 localhost: ???
+// var server = http.createServer(app).listen(3000);
+
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app).listen(80);
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host :  'localhost',
+  user :   'root',
+  password  : '0000',
+  database  : 'web'
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/get', (req, res) => {
+    res.send('get 정보 전달 잘 됨.')
+});
+app.post('/post', (req, res) => {
+    res.send('post 정보 전달 잘 됨.')
+});
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
+
+app.get('/report', function(req,res){
+  res.sendfile("./html/report.html")
+});
